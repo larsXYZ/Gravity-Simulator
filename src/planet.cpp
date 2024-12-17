46ab4f3556fa9d225cc22bab63bb5b9014196883
LarsXYZ <thehugeone@live.com>
2024-12-17 21:43:50 +0100

    cleanup

--- src/planet.cpp
+++ src/planet.cpp
@@ -27,9 +27,10 @@ double Planet::getmass() const
 	return mass;
 }
 
-double Planet::getDist(const Planet & forcer) const
+double Planet::getDist(const Planet& forcer) const
 {
-	return sqrt((forcer.getx() - getx())*(forcer.getx() - getx()) + (forcer.gety() - gety()) * (forcer.gety() - gety()));
+	return sqrt(
+		(forcer.getx() - getx()) * (forcer.getx() - getx()) + (forcer.gety() - gety()) * (forcer.gety() - gety()));
 }
 
 double Planet::getRad() const
@@ -60,7 +61,7 @@ void Planet::mark(double i)
 
 std::string Planet::getFlavorTextLife() const
 {
-	switch ((int) getLife().getTypeEnum())
+	switch (static_cast<int>(getLife().getTypeEnum()))
 	{
 	case (0):
 		{
@@ -68,31 +69,38 @@ std::string Planet::getFlavorTextLife() const
 		}
 	case (1):
 		{
-			return "Organisms that consist of one cell. The first form of life.\nOften lives in fluids in, on or under the surface.";
+			return
+				"Organisms that consist of one cell. The first form of life.\nOften lives in fluids in, on or under the surface.";
 		}
 	case (2):
 		{
-			return "Aggregate from either cell division or individuals cells\ncoming togheter. The next step in the evolution of life.";
+			return
+				"Aggregate from either cell division or individuals cells\ncoming togheter. The next step in the evolution of life.";
 		}
 	case (3):
 		{
-			return "Enormous numbers of cells work togheter to\nsupport a sizable lifeform. These can often be found\nroaming the surface of the planet.";
+			return
+				"Enormous numbers of cells work togheter to\nsupport a sizable lifeform. These can often be found\nroaming the surface of the planet.";
 		}
 	case (4):
 		{
-			return "The organisms have developed intelligence and are banding\ntogheter in groups. Often using simple technology.";
+			return
+				"The organisms have developed intelligence and are banding\ntogheter in groups. Often using simple technology.";
 		}
 	case (5):
 		{
-			return "The organisms are now the dominant species on the planet.\nThey have created advanced technology and culture.";
+			return
+				"The organisms are now the dominant species on the planet.\nThey have created advanced technology and culture.";
 		}
 	case (6):
 		{
-			return "The organisms technology has enabled them to spread to other planets.\nOnly a truly devestating event can end their civilization now.";
+			return
+				"The organisms technology has enabled them to spread to other planets.\nOnly a truly devestating event can end their civilization now.";
 		}
 	case (7):
 		{
-			return "An outpost made by the organisms. With time it will\ngrow to a fully capable part of the civilization.";
+			return
+				"An outpost made by the organisms. With time it will\ngrow to a fully capable part of the civilization.";
 		}
 	default:
 		{
@@ -103,10 +111,11 @@ std::string Planet::getFlavorTextLife() const
 
 //SIMULATION FUNCTIONS
 
-void Planet::updateVel(const Planet & forcer, double timeStep)
+void Planet::updateVel(const Planet& forcer, double timeStep)
 {
 	double aks = 0;
-	double distanceSquared = (forcer.getx() - getx())*(forcer.getx() - getx()) + (forcer.gety() - gety()) * (forcer.gety() - gety());
+	double distanceSquared = (forcer.getx() - getx()) * (forcer.getx() - getx()) + (forcer.gety() - gety()) * (forcer.
+		gety() - gety());
 	double angle = atan2(forcer.gety() - gety(), forcer.getx() - getx());
 	if (distanceSquared != 0) aks = G * forcer.getmass() / (distanceSquared);
 
@@ -118,7 +127,6 @@ void Planet::updateVel(const Planet & forcer, double timeStep)
 
 	xv += cos(angle) * aks * timeStep;
 	yv += sin(angle) * aks * timeStep;
-
 }
 
 void Planet::updateTemp()
@@ -126,7 +134,7 @@ void Planet::updateTemp()
 	temperature = temp();
 }
 
-void Planet::move(double timeStep)	
+void Planet::move(double timeStep)
 {
 	x += xv * timeStep;
 	y += yv * timeStep;
@@ -135,28 +143,24 @@ void Planet::move(double timeStep)
 
 void Planet::updateRadiAndType()
 {
-
 	if (mass < ROCKYLIMIT)
 	{
 		planetType = ROCKY;
 		density = 0.5;
 		circle.setOutlineThickness(0);
 		circle.setPointCount(30);
-
 	}
 	else if (mass < TERRESTIALLIMIT)
 	{
 		planetType = TERRESTIAL;
 		density = 0.5;
 		circle.setPointCount(40);
-
 	}
 	else if (mass < GASGIANTLIMIT)
 	{
 		planetType = GASGIANT;
 		density = 0.3;
 		circle.setPointCount(50);
-
 	}
 	else if (mass < SMALLSTARLIMIT)
 	{
@@ -230,23 +234,24 @@ void Planet::printInfo() const
 
 void Planet::printInfoShort() const
 {
-	std::cout << "ID: " << getId() << " // " << "M: " << getmass() << "    X: " << getx() << "    | " << getxv() << "    Y: " << gety() << "    | " << getyv() << "    | " << std::endl;
+	std::cout << "ID: " << getId() << " // " << "M: " << getmass() << "    X: " << getx() << "    | " << getxv() <<
+		"    Y: " << gety() << "    | " << getyv() << "    | " << std::endl;
 }
 
-void Planet::collision(const Planet & p)
+void Planet::collision(const Planet& p)
 {
-	xv = (mass*xv + p.mass*p.getxv()) / (mass + p.getmass());
-	yv = (mass*yv + p.mass*p.getyv()) / (mass + p.getmass());
+	xv = (mass * xv + p.mass * p.getxv()) / (mass + p.getmass());
+	yv = (mass * yv + p.mass * p.getyv()) / (mass + p.getmass());
 
 	double dXV = xv - p.getxv();
 	double dYV = yv - p.getyv();
 
-	incTEnergy(COLLISION_HEAT_MULTIPLIER*((dXV*dXV + dYV*dYV)*p.getmass()));
+	incTEnergy(COLLISION_HEAT_MULTIPLIER * ((dXV * dXV + dYV * dYV) * p.getmass()));
 }
 
-void Planet::draw(sf::RenderWindow &w, double xx, double yy)
+void Planet::draw(sf::RenderWindow& w, double xx, double yy)
 {
-	circle.setPosition(x-xx,y-yy);
+	circle.setPosition(x - xx, y - yy);
 	if (planetType != GASGIANT)
 	{
 		w.draw(circle);
@@ -260,7 +265,8 @@ void Planet::draw(sf::RenderWindow &w, double xx, double yy)
 
 			//SETTING FEATURES
 			int midlLines = (numAtmoLines - 1);
-			atmoLine.setRadius(circle.getRadius() - i * i * i * circle.getRadius() / (midlLines*midlLines*midlLines));
+			atmoLine.setRadius(
+				circle.getRadius() - i * i * i * circle.getRadius() / (midlLines * midlLines * midlLines));
 			atmoLine.setOrigin(atmoLine.getRadius(), atmoLine.getRadius());
 			atmoLine.setPosition(circle.getPosition());
 			atmoLine.setOutlineThickness(0);
@@ -290,13 +296,14 @@ void Planet::setColor()
 	{
 		circle.setFillColor(getStarCol());
 
-		circle.setOutlineColor(sf::Color(circle.getFillColor().r, circle.getFillColor().g, circle.getFillColor().b, 20));
+		circle.setOutlineColor(sf::Color(circle.getFillColor().r, circle.getFillColor().g, circle.getFillColor().b,
+		                                 20));
 	}
 	else if (planetType == ROCKY || planetType == TERRESTIAL)
 	{
-		double r = 100 + randBrightness +temperature / 10;
-		double g = 100 + randBrightness -temperature / 15 + getLife().getBmass() /20;
-		double b = 100 + randBrightness -temperature / 15;
+		double r = 100 + randBrightness + temperature / 10;
+		double g = 100 + randBrightness - temperature / 15 + getLife().getBmass() / 20;
+		double b = 100 + randBrightness - temperature / 15;
 
 		if (r > 255) r = 255;
 		if (g > 255) g = 255;
@@ -310,9 +317,9 @@ void Planet::setColor()
 	}
 	else if (planetType == GASGIANT)
 	{
-		double r = atmoCol_r +temperature / 10;
-		double g = atmoCol_g -temperature / 15;
-		double b = atmoCol_b -temperature / 15;
+		double r = atmoCol_r + temperature / 10;
+		double g = atmoCol_g - temperature / 15;
+		double b = atmoCol_b - temperature / 15;
 
 		if (r > 255) r = 255;
 		if (g > 255) g = 255;
@@ -339,14 +346,18 @@ std::string convertDoubleToString(double number)
 
 std::string Planet::genName()
 {
-	std::vector<std::string> navn_del_en = { "Jup", "Jor", "Ear", "Mar", "Ven", "Cer", "Sat", "Pl", "Nep", "Ur", "Ker", "Mer", "Jov", "Qur", "Deb", "Car", "Xet", "Nayt", "Erist", "Hamar", "Bjork", "Deat", "Limus", "Lant", "Hypor", "Hyper", "Tell", "It", "As", "Ka", "Po", "Yt", "Pertat" };
-	std::vector<std::string> navn_del_to = { "it", "enden", "orden", "eptux", "atur", "oper", "uqtor", "axax" };
-	std::vector<std::string> navn_del_tre = {"er", "us" , "o", "i", "atara", "ankara", "oxos", "upol", "ol", "eq"};
+	std::vector<std::string> navn_del_en = {
+		"Jup", "Jor", "Ear", "Mar", "Ven", "Cer", "Sat", "Pl", "Nep", "Ur", "Ker", "Mer", "Jov", "Qur", "Deb", "Car",
+		"Xet", "Nayt", "Erist", "Hamar", "Bjork", "Deat", "Limus", "Lant", "Hypor", "Hyper", "Tell", "It", "As", "Ka",
+		"Po", "Yt", "Pertat"
+	};
+	std::vector<std::string> navn_del_to = {"it", "enden", "orden", "eptux", "atur", "oper", "uqtor", "axax"};
+	std::vector<std::string> navn_del_tre = {"er", "us", "o", "i", "atara", "ankara", "oxos", "upol", "ol", "eq"};
 
 
-	int selectorOne = modernRandomWithLimits(0, navn_del_en.size()-1);
-	int selectorTwo = modernRandomWithLimits(0, navn_del_to.size()-1);
-	int selectorThree = modernRandomWithLimits(0, navn_del_tre.size()-1);
+	int selectorOne = modernRandomWithLimits(0, navn_del_en.size() - 1);
+	int selectorTwo = modernRandomWithLimits(0, navn_del_to.size() - 1);
+	int selectorThree = modernRandomWithLimits(0, navn_del_tre.size() - 1);
 
 	std::string number = "";
 
